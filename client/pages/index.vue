07ed165aaae1b4e1fdbf410ecde1af737577f0ae
maieulChevalier <maieul.chevalier@gmail.com>
2022-07-24 00:58:09 +0200

    Ajout de la page interface/jeu avec design sans les actions

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -14,11 +14,11 @@ function escapeHtml(unsafe) {
 <template>
   <div>
     <CustomTypedJS
-      msg="Bonjour l'artiste ! Nous avons besoin de toi pour apprendre à notre IA à coder dans les règles de l'art. ^1000 <br> Pour ce faire, nous avons créé un jeu où il te faudra battre notre IA dans une compétition acharnée pour terminer un programme avant elle. ^1000 <br> Mais Attention ! Il faut que le code soit impéccable ! Car coder vite, c'est bien... Mais coder bien, c'est mieux ! ^1000 Nous ne voudrions pas que notre IA se mette à coder n'importe quoi ! ^1000 <br> Tu es partant ? Super ! Voici les commandes qui te permettront d'écrire le programme : ^1000 <br> <u>{code: bien}</u>  ^1000 <br> <u>{code: vite}</u>  ^1000 <br> <u>{DEBUG}</u> ^1000 <br> <br> Tu n'as plus qu'à cliquer sur START une fois que tu es prêt(e)."
+      msg="Bonjour l'artiste ! Nous avons besoin de toi pour apprendre à notre IA à coder dans les règles de l'art. ^1000 <br> Pour ce faire, nous avons créé un jeu où il te faudra battre notre IA dans une compétition acharnée pour terminer un programme avant elle. ^1000 <br> Mais Attention ! Il faut que le code soit impéccable ! Car coder vite, c'est bien... Mais coder bien, c'est mieux ! ^1000 Nous ne voudrions pas que notre IA se mette à coder n'importe quoi ! ^1000 <br> Tu es partant ? Super ! Voici les commandes qui te permettront d'écrire le programme : ^1000 <br> <u>{code: bien}</u>  ^1000 <br> <u>{code: vite}</u>  ^1000 <br> <u>{code: debug}</u> ^1000 <br> <br> Tu n'as plus qu'à cliquer sur START une fois que tu es prêt(e)."
     />
-    <v-btn elevation="9" x-large color="primary" :to="'/interface'"
-      >START</v-btn
-    >
+    <v-btn elevation="9" x-large color="primary" :to="'/interface'">
+      START
+    </v-btn>
   </div>
 </template>
 
--- client/pages/interface.vue
+++ client/pages/interface.vue
@@ -1,28 +1,103 @@
+<script setup>
+import { onMounted } from "vue";
+
+import CustomTypedJS from "../components/CustomTypedJS.vue";
+
+// Ajouter les 4 actions
+// Ajouter couleurs au code "écrit" par l'utilisateur et l'IA
+// Protéger le TextField
+// Écrire les tests
+
+function multiply() {
+  const a = prompt();
+  const b = prompt();
+  return alert(a * b);
+}
+
+onMounted(() => {
+  // multiply();
+});
+</script>
 <template>
-  <v-row>
-    <v-col class="text-center">
-      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
-      <blockquote class="blockquote">
-        "First, solve the problem. Then, write the code."
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-col>
-    <v-col class="text-center">
-      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
-      <blockquote class="blockquote">
-        "First, solve the problem. Then, write the code."
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-col>
-  </v-row>
+  <div>
+    <v-container class="ma-1">
+      <v-row class="align-center">
+        <v-column>
+          <v-text-field
+            label="Code"
+            placeholder="{code: vite}"
+            outlined
+            hide-details="auto"
+            style="max-width: 200px; margin-right: 20px"
+          ></v-text-field>
+        </v-column>
+
+        <v-column>
+          <v-btn elevation="9" x-large color="primary" :to="'/interface'">
+            RESET
+          </v-btn></v-column
+        >
+      </v-row>
+      <v-row class="align-center"> </v-row>
+    </v-container>
+
+    <br />
+
+    <v-row>
+      <v-col>
+        <v-container>
+          <v-icon x-large>mdi-tooltip-account</v-icon>
+          <br />
+          <br />
+          <v-progress-linear
+            background-color="red lighten-2"
+            color="green lighten-2"
+            value="15"
+            buffer-value="30"
+            stream
+          ></v-progress-linear>
+          <br />
+          <CustomTypedJS
+            msg="function multiply() {<br/> &nbsp;&nbsp;&nbsp; const a = prompt() <br/> &nbsp;&nbsp;&nbsp; const b = prompt() <br/> &nbsp;&nbsp;&nbsp; return alert(a * b); <br/> } <br/> <br/> multiply()"
+          />
+          <blockquote class="blockquote">
+            "Coder vite c'est bien... Mais coder bien, c'est mieux !"
+            <footer>
+              <small>
+                <em>&mdash;John Johnson</em>
+              </small>
+            </footer>
+          </blockquote>
+        </v-container>
+      </v-col>
+      <v-divider vertical color="white"></v-divider>
+      <v-col>
+        <v-container>
+          <v-icon x-large>mdi-robot-happy-outline</v-icon>
+          <br />
+          <br />
+          <v-progress-linear
+            color="green lighten-2"
+            value="15"
+            buffer-value="0"
+            stream
+          ></v-progress-linear>
+          <br />
+          <CustomTypedJS
+            msg="function multiply() {<br/> &nbsp;&nbsp;&nbsp; const a = prompt() <br/> &nbsp;&nbsp;&nbsp; const b = prompt() <br/> &nbsp;&nbsp;&nbsp; return alert(a * b); <br/> } <br/> <br/> multiply()"
+          />
+          <blockquote class="blockquote">
+            "Coder vite c'est bien... Mais coder bien, c'est mieux !"
+            <footer>
+              <small>
+                <em>&mdash;John Johnson</em>
+              </small>
+            </footer>
+          </blockquote>
+        </v-container>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
