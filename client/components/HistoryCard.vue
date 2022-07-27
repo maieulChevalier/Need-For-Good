b0431c9294167bbc0ffee646c66469b2d81025f0
maieulChevalier <maieul.chevalier@gmail.com>
2022-07-27 22:11:30 +0200

    Ajout HistoryCard

---
+++ client/components/HistoryCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-card class="mx-auto px-2">
+    <v-card-title class="mb-2 d-flex justify-space-between">
+      <span class="text-subtitle-2">10/09/1997</span>
+      <span><strong>Victoire !</strong></span>
+    </v-card-title>
+    <v-row class="">
+      <v-col :cols="5">
+        <v-icon x-large class="d-flex justify-center">mdi-human-child</v-icon>
+        <v-container>
+          <v-progress-linear
+            background-color="red lighten-2"
+            color="green lighten-2"
+            stream
+          ></v-progress-linear>
+        </v-container>
+      </v-col>
+      <v-col :cols="2" class="text-center"> VS </v-col>
+      <v-col :cols="5">
+        <v-icon x-large class="d-flex justify-center">
+          mdi-robot-happy-outline
+        </v-icon>
+        <v-container>
+          <v-progress-linear
+            background-color="red lighten-2"
+            color="green lighten-2"
+            stream
+          ></v-progress-linear>
+        </v-container>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "HistoryPage",
+};
+</script>
--- client/pages/history.vue
+++ client/pages/history.vue
@@ -1,67 +1,19 @@
-<script setup>
-import { ref, onMounted } from "vue";
-import Typewriter from "typewriter-effect/dist/core";
-
-const typewriterRef = ref();
-let typewriter = ref();
-
-onMounted(() => {
-  typewriter = new Typewriter(typewriterRef.value, {
-    delay: 10,
-    autoStart: true,
-  });
-  typewriter
-    .typeString("Bonjour l'artiste ! ")
-    .pauseFor(1000)
-    .typeString(
-      "Nous avons besoin de toi pour apprendre à notre IA à coder sans faire d'erreurs. <br>"
-    )
-    .pauseFor(1000)
-    .typeString(
-      "Pour ce faire, nous avons créé un jeu où il te faudra battre notre IA dans une compétition acharnée pour programmer une calculatrice. <br>"
-    )
-    .pauseFor(1000)
-    .typeString("Mais Attention ! ")
-    .pauseFor(1000)
-    .typeString("Il faut que le code soit impéccable ! ")
-    .pauseFor(1000)
-    .typeString("Car coder vite, c'est bien... ")
-    .pauseFor(1000)
-    .typeString("Mais coder bien, c'est mieux ! <br>")
-    .pauseFor(1000)
-    .typeString("Tu es partant ? ")
-    .pauseFor(1000)
-    .typeString("Super ! <br>")
-    .pauseFor(1000)
-    .typeString(
-      "Voici les commandes qui te permettront d'écrire le programme : <br>"
-    )
-    .pauseFor(1000)
-    .typeString("<strong>codeCarefully()</strong> <br>")
-    .pauseFor(1000)
-    .typeString("<strong>codeQuickly()</strong> <br>")
-    .pauseFor(1000)
-
-    .typeString("<strong>debug()</strong> <br>")
-    .pauseFor(1000)
-
-    .typeString("Cliques sur START quand tu es prêt(e) 🙃.")
-    .pauseFor(1000)
-    .start();
-});
-</script>
-
 <template>
   <div>
-    <p><span ref="typewriterRef"></span></p>
-    <v-btn elevation="9" x-large color="primary" :to="'/interface'">
-      START
-    </v-btn>
+    <HistoryCard />
+    <br />
+    <HistoryCard />
+    <br />
+    <HistoryCard />
+    <br />
+    <HistoryCard />
   </div>
 </template>
 
 <script>
+import HistoryCard from "../components/HistoryCard.vue";
 export default {
   name: "HistoryPage",
+  components: { HistoryCard },
 };
 </script>
