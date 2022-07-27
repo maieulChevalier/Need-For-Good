6b97b7910db2ee2d4b438bfb1191c051db61b4dd
maieulChevalier <maieul.chevalier@gmail.com>
2022-07-27 20:05:03 +0200

    Ajout redirect vers login même en cheagant de route

--- client/plugins/route.js
+++ client/plugins/route.js
@@ -6,16 +6,13 @@ if (
   window.location.replace("/login");
 }
 
-// export default async ({ app }) => {
-//   // Every time the route changes (fired on initialization too)
-//   // const isMounted = await app.mounted()
-//   console.log("app: ", app);
-//   app.router.beforeEach((to, from, next) => {
-//     if (to.name !== "login" && localStorage.getItem("userName") === null)
-//       next({ name: "login" });
-//     else {
-//       console.log("helloooo");
-//       next();
-//     }
-//   });
-// };
+export default async ({ app }) => {
+  app.router.beforeEach((to, from, next) => {
+    if (to.name !== "login" && localStorage.getItem("userName") === null)
+      next({ name: "login" });
+    else {
+      console.log("helloooo");
+      next();
+    }
+  });
+};
